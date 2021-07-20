bfcf42e7a5e8d0a17ae11fc01e9ea483339b4bbb
cpan1995 <cpan1995@gmail.com>
2021-07-20 17:50:58 -0400

    commit tree 0e96ccc0a139b44c79ee81a24a279917a1bed355

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linksaTag = links.map(element => {
+    return <a key={element} href={`#${element}`}>{element}</a>
+  })
+  return (
+    <nav>
+      {linksaTag}
+    </nav>
+  )
+  
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,16 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  //console.log(projects)
+  const newArray = projects.map(element => {
+    //console.log(element.id)
+    return <ProjectItem key = {element.id} keyId = {element.id} name = {element.name} about = {element.about} technologies = {element.technologies} />
+  })
+  //console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{newArray}</div>
     </div>
   );
 }
